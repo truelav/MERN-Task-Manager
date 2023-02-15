485a0263a26eca4ebb43931364c2b1893a258b1f
unknown <madan.serghei@yahoo.com>
2023-02-15 16:32:16 +0200

    ADD: connected to mongoDB, need to create model schemas for db

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,20 @@
-console.log("hello server");
+import express from "express";
+import mongoose from "mongoose";
+// import dotenv/config
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// DATABASE CONNECTIONS
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DO_CONNECTION_STRING);
+    console.log("mongo DB connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
