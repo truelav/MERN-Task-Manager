bb34ef6227ee63a5f9cadc43f5da3741c1dc7fb8
unknown <madan.serghei@yahoo.com>
2023-02-16 18:56:32 +0200

    ADD: controllers for the login function was added

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,5 @@
 import bcryptjs from "bcryptjs";
-
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const register = async (req, res) => {
@@ -31,4 +31,45 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = () => {};
+export const login = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json("Missing required field email or password");
+  }
+
+  try {
+    const user = await User.findOne({ email: req.body.email }).select({
+      name,
+      email,
+      password,
+    });
+
+    if (!user) {
+      return res.status(404).json(`User ${req.body.email} was not found`);
+    }
+
+    const isPasswordCorrect = await bcryptjs.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(404).json("Password Incorrect");
+    }
+
+    const payload = {
+      id: user._id,
+      name: user.name,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("access cookie", token, {
+      httpOnly: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
