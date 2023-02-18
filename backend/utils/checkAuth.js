9e1073144488c19b571b0210756c32a1998911f1
unknown <madan.serghei@yahoo.com>
2023-02-18 18:54:02 +0200

    FEAT: replacing hardcoded error handling with the error handling function

--- backend/utils/checkAuth.js
+++ backend/utils/checkAuth.js
@@ -1,9 +1,10 @@
-import jwt, { decode } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import createError from "./error.js";
 
 export const checkAuth = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return res.status(500).json("Access Token not available");
+    return createError({ status: 401, message: "Unauthorized" });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
