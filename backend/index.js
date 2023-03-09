fffed3c19e4b8701ba3560a465c9222792893098
unknown <madan.serghei@yahoo.com>
2023-03-09 12:39:22 -0800

    connected to MongoDB Local

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,28 @@ import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 
+const PORT = process.env.PORT || 4444;
 const app = express();
+
+console.log(process.env.PORT);
+
+//Connecting to the local MongoDB
+// mongoose
+//   .connect("" + process.env.DB_CONNECTION_STRING)
+//   .then(() => console.log("Connected to DB"))
+//   .catch((err) => console.log("DB connection ", err));
+
+//Or Use the Async Await
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+
+    console.log("Connected to the DB");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 // By Default Node doesnt know how to work with JSON
 // This will make req.body read the JSON we are sending from the user
@@ -32,6 +53,6 @@ app.post("/auth/login", (req, res) => {
 
 app.listen(3000, (err) => {
   if (err) return console.log(err);
-
+  connectDB();
   console.log(`Server OK on port ${3000}`);
 });
--- backend/models/User.js
+++ backend/models/User.js
@@ -17,6 +17,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    avatarUrl: String,
   },
   { timestamps: true }
 );
