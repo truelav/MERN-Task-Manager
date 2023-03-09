e965885c7dd285bc1aefd92e0a203235d1315543
unknown <madan.serghei@yahoo.com>
2023-03-09 11:48:33 -0800

    basic application for the JWT

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
@@ -6,8 +7,27 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+// By Default Node doesnt know how to work with JSON
+// This will make req.body read the JSON we are sending from the user
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World vasilica");
+});
+
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+
+  //creating token when we are signing in
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      name: req.body.name,
+    },
+    "SecretSecret123"
+  );
+
+  res.json({ success: true });
 });
 
 app.listen(3000, (err) => {
